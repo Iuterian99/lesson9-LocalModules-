ddae616293898bb7a6fcfeb1f55273e32f90695d
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-06 22:06:34 +0500

    started local modules

---
+++ locals.js
@@ -0,0 +1,3 @@
+module.exports.log = (arg) => {
+  console.log(arg);
+};
---
+++ server.js
@@ -0,0 +1,6 @@
+//! LOCAL MODULES
+// local modullar bu biz qo`lda tayyorlagan reusable (qayta-qayta ishlatiladigan) modullar!
+
+const { log: myLog } = require("./locals");
+
+myLog("salom");
