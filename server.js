e4fd6cd5be45abf1c904c5071037ec755398a9a8
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-07 00:18:38 +0500

    Local modules & package.json

--- server.js
+++ server.js
@@ -33,5 +33,8 @@
 /* package.json ---> backend da dasturchi o`zi tuzishi kerak bo`lgan dasturi haqida to`liq (dastur nomi, joylashuvi, aftori, ishlatgan versiyalari ...) ma`lumot saqlanadigan qism! 
 package.json kirish orqali proekt haqida 60-70% ma`lumot ovolsa bo`ladi! 
 !package.json ni qo`lda yozish uchun quyidagilar qilinadi -> 
-1) cmd terminalda "npm init" yoziladi!
+1) cmd terminalda "npm init" yozsak ma`lumotlani qo`lda o`zimiz to`ldirib chiqishimiza kk!
+2) cmd terminalda "npm init -y" desak to`ldirilgan ma`lumotlarni tayyor chiqarib beradi
+
+note: cmd da "npm i nodemon --save-dev"  ---> deganimizada package.jsonga nodemonni "devDependencies" degan bo`limiga joylashtiradi va bu proektimiz davomida live server har bir o`zgarishni kuzatuvchi vazifasini bajaradi! 
 */
